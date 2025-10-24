77d31b95a185270cd5e8788840515be0d3755076
smarg1 <ayaanblr.hussain@gmail.com>
2025-10-24 21:00:57 +0530

    sitemap_fix

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { slugify } from "@/utils/Slugify";
 import { getBlogs, getWorks } from "@/utils/fetchData";
 import type { Blog, Work } from "@/utils/fetchData";
 
-const BASE_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL || "";
+const BASE_URL = `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}` || "";
 
 function calculatePriority(path: string) {
   const depth = path.split("/").filter(Boolean).length;
